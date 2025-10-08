f6fc7caad32cd95880d4cd5941ce82c16e508002
thedevlucxs <dev.lucxs@gmail.com>
2025-10-08 08:51:20 -0300

    Front e back integrados

--- Backend/app/Models/Post.php
+++ Backend/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -18,4 +19,8 @@ class Post extends Model
         'user_id',
     ];
 
+public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
