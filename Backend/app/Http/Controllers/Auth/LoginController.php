38d44a60b0b077c306924bcb6feb4a2fcfeb03d3
thedevlucxs <dev.lucxs@gmail.com>
2025-10-14 09:49:38 -0300

    fix: Corrige o erro de digitação no nome da variável access_token

--- Backend/app/Http/Controllers/Auth/LoginController.php
+++ Backend/app/Http/Controllers/Auth/LoginController.php
@@ -36,7 +36,7 @@ class LoginController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
-            'user' => $request->user(),
+            'user' => $user,
         ]);
     }
     public function logout(Request $request)
