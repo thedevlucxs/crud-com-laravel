20510a22e8cda72c1f193e577395b2eb6582ab11
thedevlucxs <dev.lucxs@gmail.com>
2025-10-23 09:11:01 -0300

    fix(ui): Limpa layout removendo tags <hr>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -172,7 +172,6 @@ function App() {
   return (
     <div className="container mx-auto p-4 max-w-5x1">
       <h1 className="text-3x1 font-bold text-center mb-6">Blog com React e Laravel</h1>
-      <hr className="mb-6" />
       {selectedPost ? (
         <PostDetail
           post={selectedPost}
@@ -187,13 +186,13 @@ function App() {
       ) : (
         <>
           {!token ? (
-            <div className="flex items-center justify-center min-h-[calc(100vh-10rem)]">
+            <div className="flex items-center justify-center">
             <LoginForm onLoginSuccess={handleLoginSuccess} />
             </div>
           ) : (
-            <div className="bg-card text-card-foreground p-4 rounded-lg shadow-md mb-6 flex justify-between items-center">
+            <div className="text-card-foreground p-4 rounded-lg mb-6 flex justify-between items-center">
               <p>
-                <strong>Login efetuado com sucesso!</strong>
+                <strong>Bem vindo!</strong>
               </p>
               <div className="flex gap-2">
               <Button aria-label="submit" onClick={fetchPosts} >
@@ -212,7 +211,6 @@ function App() {
           {error && <p className="text-destructive bg-destructive/10 p-3 rounded-md mt-4 text-center">{error}</p>}
           {token && (
             <>
-              <hr className="my-6" />
               {editingPost ? (
                 <EditPostForm
                   editingPost={editingPost}
