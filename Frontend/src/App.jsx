9b6e5b0480b64d3cf8504202bb606dc98652437a
thedevlucxs <dev.lucxs@gmail.com>
2025-10-23 11:04:03 -0300

    Revert "feat: Implementa separator do shadcn no App.jsx"
    This reverts commit 1de5e08f11c0f5707c8e6cf7429d870c292c891a.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,8 +17,7 @@ import CreatePostForm from "./components/CreatePostForm";
 import EditPostForm from "./components/EditPostForm";
 import "./App.css";
 
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
+import {Button} from "@/components/ui/button"
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token") || null);
@@ -171,30 +170,8 @@ function App() {
   };
 
   return (
-    <div className="container mx-auto p-4 max-w-5xl">
-    <header>
-      <div className="flex justify-between items-center mb-4">
-        <h1 className="text-3xl font-bold">Blog com React e Laravel</h1>
-        <div className="flex items-center gap-4">
-          <p>
-            <strong>Bem vindo!</strong>
-          </p>
-          <div className="flex gap-2">
-            <Button aria-label="submit" onClick={fetchPosts} >
-              Buscar Posts
-            </Button>
-            <Button
-              type="button"
-              onClick={handleLogout}
-              variant="destructive"
-              >
-              Sair (Logout)
-            </Button>
-          </div>
-        </div>
-      </div>  
-      <Separator className="mb-6" />
-    </header>
+    <div className="container mx-auto p-4 max-w-5x1">
+      <h1 className="text-3x1 font-bold text-center mb-6">Blog com React e Laravel</h1>
       {selectedPost ? (
         <PostDetail
           post={selectedPost}
@@ -208,16 +185,32 @@ function App() {
         />
       ) : (
         <>
-          {!token && (
+          {!token ? (
             <div className="flex items-center justify-center">
             <LoginForm onLoginSuccess={handleLoginSuccess} />
             </div>
+          ) : (
+            <div className="text-card-foreground p-4 rounded-lg mb-6 flex justify-between items-center">
+              <p>
+                <strong>Bem vindo!</strong>
+              </p>
+              <div className="flex gap-2">
+              <Button aria-label="submit" onClick={fetchPosts} >
+                Buscar Posts
+              </Button>
+              <Button
+                type="button"
+                onClick={handleLogout}
+                variant="destructive"
+              >
+                Sair (Logout)
+              </Button>
+              </div>
+            </div>
           )}
-
           {error && <p className="text-destructive bg-destructive/10 p-3 rounded-md mt-4 text-center">{error}</p>}
           {token && (
             <>
-              <hr className="my-6" />
               {editingPost ? (
                 <EditPostForm
                   editingPost={editingPost}
